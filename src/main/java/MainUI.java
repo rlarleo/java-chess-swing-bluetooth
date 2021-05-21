ebb8e1c112f58ec5bce77e966f7560261378665f
김기대 <soulen2403@naver.com>
2021-05-21 17:46:27 +0900

    1. MainUI 2. Extract Method 3. Improved readability

--- src/main/java/MainUI.java
+++ src/main/java/MainUI.java
@@ -246,7 +246,6 @@ public class MainUI extends JFrame {
             if (e.getSource() == newgame) {
                 mainpanel.setVisible(false);
                 subpanel.setVisible(true);
-
             }
 
             if (e.getSource() == options) {
@@ -440,61 +439,34 @@ public class MainUI extends JFrame {
 
         logo.setIcon(bluechess);
         logo.setText(null);
-
-        newgame.setIcon(newgame1);
-        newgame.setText(null);
-        newgame.setToolTipText("New Game");
-
-
+        
+        setMenu(newgame, newgame1, "New Game");
         newgame.addMouseListener(handler);
-
-        options.setIcon(options1);
-        options.setText(null);
-        options.setToolTipText("Options");
-
+        
+        setMenu(options, options1, "Options");
         options.addMouseListener(handler);
-
-        help.setIcon(help1);
-        help.setText(null);
-        help.setToolTipText("Help");
-
+        
+        setMenu(help, help1, "Help");
         help.addMouseListener(handler);
 
-        about.setIcon(about1);
-        about.setText(null);
-        about.setToolTipText("About Game");
-
+        setMenu(about, about1, "About Game");
         about.addMouseListener(handler);
-
-        exit.setIcon(exit1);
-        exit.setText(null);
-        exit.setToolTipText("Exit From Game");
-
+        
+        setMenu(exit, exit1, "Exit From Game");
         exit.addMouseListener(handler);
 
-        single.setIcon(single1);
-        single.setText(null);
-        single.setToolTipText("Play in same device");
-
+        setMenu(single, single1, "Play in same device");
         single.addMouseListener(handler);
 
-        join.setIcon(join1);
-        join.setText(null);
-        join.setToolTipText("Join to play through BLUETOOTH");
-
+        setMenu(join, join1, "Join to play through BLUETOOTH");
         join.addMouseListener(handler);
 
-        host.setIcon(host1);
-        host.setText(null);
-        host.setToolTipText("Host to play through BLUETOOTH");
-
+        setMenu(host, host1, "Host to play through BLUETOOTH");
         host.addMouseListener(handler);
 
-        back.setIcon(back1);
-        back.setText(null);
-        back.setToolTipText("Back to MainMenu");
-
+        setMenu(back, back1, "Host to play through BLUETOOTH");
         back.addMouseListener(handler);
+
 
         Point menuCenter = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
         //adjust the bound of frame by a simple logic
@@ -509,4 +481,11 @@ public class MainUI extends JFrame {
         setVisible(true);
 
     }
+
+
+	private void setMenu(JLabel menuName, Icon menuIcon, String tipText) {
+		menuName.setIcon(menuIcon);
+		menuName.setText(null);
+		menuName.setToolTipText(tipText);
+	}
 }
