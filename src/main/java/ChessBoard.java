cdc92550c766de93d125d458295fdb54756a2f9e
김기대 <soulen2403@naver.com>
2021-05-21 17:51:18 +0900

    1. ChessBoard 2. Extract Method, Extract Variable, Rename 3. Improved readability and Understandability

--- src/main/java/ChessBoard.java
+++ src/main/java/ChessBoard.java
@@ -7,8 +7,12 @@ import java.util.Vector;
 
 public class ChessBoard extends Canvas {
 
+    private final static Color white = new Color(255,255,255);
+    private final static Color ocher = new Color(165, 130, 95);
+    //private final static Color grey = new Color(51, 51, 51);
     protected PaintInstruction currentInstruction = null;
     protected final Vector<PaintInstruction> vecPaintInstructions = new Vector<>();
+    private final static int numberOfTiles = 8;
 
     public void update(Graphics g) {
         System.err.println("chessboard, update() being running");
@@ -19,7 +23,7 @@ public class ChessBoard extends Canvas {
         System.err.println("chessboard, paint() being running");
         if (vecPaintInstructions.size() == 0) {
 
-            currentInstruction = new PaintInstruction(0, 0, 8);
+            currentInstruction = new PaintInstruction(0, 0, numberOfTiles);
             vecPaintInstructions.addElement(currentInstruction);
 
         }
@@ -46,41 +50,35 @@ public class ChessBoard extends Canvas {
         drawExtra(g);
 
     }
-
+    
     private void drawTile(int row, int column, Graphics g) {
         //todo: add the cell labels A-H:1-8
-        if ((row + 1) % 2 == 0) {
+    	//refactoring, Extract variable
+    	boolean row_column_odd = ((row + 1) % 2 == 0)&&((column + 1) % 2) == 0;
+    	boolean row_column_even = (row % 2 == 0)&&(column % 2 == 0);
+        boolean row_odd_column_even = ((row + 1) % 2 == 0)&&(column % 2 == 0);
+        boolean row_even_column_odd = (row % 2 == 0)&&((column + 1) % 2 == 0);
+    			
+        if(row_column_odd || row_column_even) 
+        	g.setColor(white); //white
+        if(row_odd_column_even || row_even_column_odd) 
+            g.setColor(ocher); //ocher
 
-            if ((column + 1) % 2 == 0) {
-                g.setColor(new Color(255, 255, 255)); //white
-            } else {
-                g.setColor(new Color(165, 130, 95)); //black
-            }
-
-        } else {
-
-            if ((column + 1) % 2 == 0) {
-                g.setColor(new Color(165, 130, 95));
-            } else {
-                g.setColor(new Color(255, 255, 255));
-            }
-
-        }
-
-        g.fillRect((5 + (column * 50)), (5 + (row * 50)), 50, 50);
+        g.fillRect(coordinateToTile(column), coordinateToTile(row), 50, 50);
 
     }
 
     protected void drawExtra(Graphics g) //Any class extending the chess board can use this method to add extra things like player pieces
     {
     }
+    
 
-    protected int findWhichTileSelected(int coor) //Finds which tile the mouse is over
+    protected int findWhichTileSelected(int coordinate) //Finds which tile the mouse is over
     {
         System.err.println("chessboard, findwhichtileselected() being running");
-        for (int i = 0; i < 8; i++) {
-
-            if ((coor >= (5 + (i * 50))) && (coor <= (55 + (i * 50)))) {
+        for (int i = 0; i < numberOfTiles; i++) {
+        	
+            if (isSelectedTile(i,coordinate)) {
                 return i;
             }
 
@@ -88,5 +86,13 @@ public class ChessBoard extends Canvas {
 
         return -1;
 
+    }
+    
+    private int coordinateToTile(int _coordinate) {
+    	return 5 + (_coordinate * 50);
+    }
+    
+    private boolean isSelectedTile(int i, int _coordinate) {
+    	return (_coordinate >= (5 + (i * 50))) && (_coordinate <= (55 + (i * 50)));
     }
 }
