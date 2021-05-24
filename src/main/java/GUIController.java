e4a6d3deef253d9de7901d26d73d5251b115eb67
김기대 <soulen2403@naver.com>
2021-05-24 21:16:36 +0900

    1. MainUI, GUIController 김기대 2. Facade Pattern, Extract Method 3. 새로운 모드 개발 시 모드들을 하나로 묶어주는 역할을 하는 클래스를 생성해놓음으로써 유지보수성 향상

---
+++ src/main/java/GUIController.java
@@ -0,0 +1,19 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class GUIController{
+	
+	public GUISingle singleChessWindow;
+	public GUIBT BTChessWindow;
+	
+    public void makeSingleChessWindow(JFrame main) {
+    	singleChessWindow = new GUISingle();
+    	singleChessWindow.createGUI(main);
+    }
+    
+    public void makeBTChessWindow(JFrame main) {
+    	BTChessWindow = new GUIBT(false);
+        BTChessWindow.createGUI(main);
+
+    }
+}
